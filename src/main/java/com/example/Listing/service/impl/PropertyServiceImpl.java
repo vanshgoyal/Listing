f92b68f11bc833ae68beb2e244c2ebab1efc612e
Your Name <vansh.goyal@nobroker.in>
2022-05-30 12:47:06 +0530

    update added for property mass

--- src/main/java/com/example/Listing/service/impl/PropertyServiceImpl.java
+++ src/main/java/com/example/Listing/service/impl/PropertyServiceImpl.java
@@ -1,11 +1,15 @@
 package com.example.Listing.service.impl;
 
+import com.example.Listing.model.ParamModel;
 import com.example.Listing.model.Property;
 import com.example.Listing.model.mass_model;
 import com.example.Listing.repository.RepositoryProperty.PropertyRepository;
 import com.example.Listing.service.PropertyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 /**
@@ -21,11 +25,29 @@ public class  PropertyServiceImpl implements PropertyService {
 
     @Override
     public mass_model saveOrUpdateProperty(Property property) {
-        mass_model property_mass = new mass_model();
-        property_mass.setId(property.getId());
-        property_mass.setPropertyId(property.getPropertyId());
-        property_mass.setMassVal(property.getDeposit()* property.getLeaseType()* property.getPhotoCount());
-        return propertyRepository.save(property_mass);
+        if(propertyRepository.findBypropertyId(property.getPropertyId())!= null)
+        {
+//            Query query = new Query();
+//            Update update = new Update().inc("paramType", paramModel.getParamType());
+            Query query = new Query(
+                    Criteria.where("propertyId").is(property.getPropertyId()));
+            System.out.println(query);
+            mass_model property_mass = new mass_model();
+            property_mass.setId(property.getId());
+            property_mass.setPropertyId(property.getPropertyId());
+            property_mass.setMassVal(property.getDeposit()* property.getLeaseType()* property.getPhotoCount());
+            Update update = new Update().set("massVal", property_mass.getMassVal());
+
+            return mt.findAndModify(query, update, mass_model.class);
+        }
+        else {
+            mass_model property_mass = new mass_model();
+            property_mass.setId(property.getId());
+            property_mass.setPropertyId(property.getPropertyId());
+            property_mass.setMassVal(property.getDeposit()* property.getLeaseType()* property.getPhotoCount());
+            return propertyRepository.save(property_mass);
+        }
+
     }
 
     @Override
