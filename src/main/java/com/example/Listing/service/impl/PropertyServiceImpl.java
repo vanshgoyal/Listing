16a709fd9cc6af7b362b90e81e19f6a2dfafc311
Your Name <vansh.goyal@nobroker.in>
2022-05-30 13:43:33 +0530

    added param-mass system

--- src/main/java/com/example/Listing/service/impl/PropertyServiceImpl.java
+++ src/main/java/com/example/Listing/service/impl/PropertyServiceImpl.java
@@ -4,13 +4,19 @@ import com.example.Listing.model.ParamModel;
 import com.example.Listing.model.Property;
 import com.example.Listing.model.mass_model;
 import com.example.Listing.repository.RepositoryProperty.PropertyRepository;
+import com.example.Listing.service.ParamService;
 import com.example.Listing.service.PropertyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.Future;
 
 /**
  * @author ragcrix
@@ -22,6 +28,8 @@ public class  PropertyServiceImpl implements PropertyService {
     private MongoTemplate mt;
     @Autowired
     private PropertyRepository propertyRepository;
+    @Autowired
+    private ParamService paramService;
 
     @Override
     public mass_model saveOrUpdateProperty(Property property) {
@@ -35,7 +43,10 @@ public class  PropertyServiceImpl implements PropertyService {
             mass_model property_mass = new mass_model();
             property_mass.setId(property.getId());
             property_mass.setPropertyId(property.getPropertyId());
-            property_mass.setMassVal(property.getDeposit()* property.getLeaseType()* property.getPhotoCount());
+
+            ParamModel paramModel = new ParamModel();
+            paramModel = paramService.findBycityId("1");
+            property_mass.setMassVal(property.getDeposit()* paramModel.getParamDeposit());
             Update update = new Update().set("massVal", property_mass.getMassVal());
 
             return mt.findAndModify(query, update, mass_model.class);
@@ -44,7 +55,10 @@ public class  PropertyServiceImpl implements PropertyService {
             mass_model property_mass = new mass_model();
             property_mass.setId(property.getId());
             property_mass.setPropertyId(property.getPropertyId());
-            property_mass.setMassVal(property.getDeposit()* property.getLeaseType()* property.getPhotoCount());
+
+            ParamModel paramModel = new ParamModel();
+            paramModel = paramService.findBycityId("1");
+            property_mass.setMassVal(property.getDeposit()* paramModel.getParamDeposit());
             return propertyRepository.save(property_mass);
         }
 
