b84edcd639b44a412b981064e05eba39589d341d
Your Name <vansh.goyal@nobroker.in>
2022-06-02 12:49:47 +0530

    apiCall method added

--- src/main/java/com/example/Listing/service/impl/RestClientServiceImpl.java
+++ src/main/java/com/example/Listing/service/impl/RestClientServiceImpl.java
@@ -5,7 +5,6 @@ import com.example.Listing.service.RestClientService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -22,30 +21,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class RestClientServiceImpl implements RestClientService
 {
-    Logger logger = LoggerFactory.getLogger(RestClientServiceImpl.class);
+    static Logger logger = LoggerFactory.getLogger(RestClientServiceImpl.class);
     public PropertyDTO getPropertyDTO(String propertyId) {
 
         String uri = "https://www.nobroker.in/api/v1/property/"+propertyId;
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpget = new HttpGet(uri);
-        HttpResponse httpresponse = null;
-
-        try {
-            httpresponse = httpclient.execute(httpget);
-        } catch (IOException e) {
-            logger.error("Unable to get property params");
-            throw new RuntimeException(e);
-        }
+        Scanner sc = apiCall(uri);
 
         Gson gson=new Gson();
-        Scanner sc = null;
-        try {
-            sc = new Scanner(httpresponse.getEntity().getContent());
-        } catch (IOException e) {
-            logger.error("Unable to get response from property ");
-            throw new RuntimeException(e);
-        }
-
         String json = sc.nextLine();
         JsonObject body = gson.fromJson(json, JsonObject.class);
         String newstr = (body.get("data").toString());
@@ -63,5 +45,26 @@ public class RestClientServiceImpl implements RestClientService
 
         return (propertyDTO);
 
+    }
+    private static Scanner apiCall(String uri)
+    {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpget = new HttpGet(uri);
+        HttpResponse httpresponse = null;
+
+        try {
+            httpresponse = httpclient.execute(httpget);
+        } catch (IOException e) {
+            logger.error("Unable to get property params");
+            throw new RuntimeException(e);
+        }
+        Scanner sc = null;
+        try {
+            sc = new Scanner(httpresponse.getEntity().getContent());
+        } catch (IOException e) {
+            logger.error("Unable to get response from property ");
+            throw new RuntimeException(e);
+        }
+        return sc;
     }
 }
